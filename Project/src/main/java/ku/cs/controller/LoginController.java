f853aeb107ca92b9e3b08bccc6e4603268eb1f61
B_n <78348022+boonblablabla@users.noreply.github.com>
2022-11-03 00:40:39 +0700

    make module export to javafx.fxml and javafx.graphics

--- Project/src/main/java/ku/cs/controller/LoginController.java
+++ Project/src/main/java/ku/cs/controller/LoginController.java
@@ -15,7 +15,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class LoginControlller {
+public class LoginController {
     @FXML
     private TextField usernameTextField;
     @FXML
--- Project/src/main/java/module-info.java
+++ Project/src/main/java/module-info.java
@@ -1,10 +1,16 @@
 module appDBConnect {
-    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens ku.cs to javafx.fxml;
-    exports ku.cs;
+    exports ku.cs to javafx.fxml, javafx.graphics;
+
+    exports ku.cs.controller to javafx.fxml;
+    opens ku.cs.controller to javafx.fxml;
+
+    exports ku.cs.service to javafx.fxml;
+    opens ku.cs.service to javafx.fxml;
 }
