135abe0296b3585c909d073f2bddb56dd35b6d4c
B_n <78348022+boonblablabla@users.noreply.github.com>
2022-11-03 00:42:28 +0700

    Update DatabaseConnection.java

--- Project/src/main/java/ku/cs/connector/DatabaseConnection.java
+++ Project/src/main/java/ku/cs/connector/DatabaseConnection.java
@@ -1,32 +1,17 @@
 package ku.cs.connector;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseConnection {
-    public Connection databaseLink;
-    public Connection getConnection() {
-        String databaseName = "";
+    private static Connection databaseLink;
+    public static Connection connect(String databaseName) {
         String databaseUser = "";
         String databasePassword = "";
-        String url = "jdbc:mysql://localhost:3306/" + databaseName;
+        String url = "jdbc:mysql://localhost/" + databaseName;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
             databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
-
-            Statement statement = databaseLink.createStatement();
-
-//            ResultSet resultSet = statement.executeQuery("select * from ")
-//
-//            while (resultSet.next()) {
-//
-//            }
-
-            databaseLink.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
