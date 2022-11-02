a2965c68dd73253939cef155c76885ac2accddab
B_n <78348022+boonblablabla@users.noreply.github.com>
2022-11-02 22:46:43 +0700

    Update Order and OrderList
    update Order and OrderList models

--- Project/src/main/java/ku/cs/model/Order.java
+++ Project/src/main/java/ku/cs/model/Order.java
@@ -1,4 +1,45 @@
 package ku.cs.model;
 
 public class Order {
+    private int oid;
+    private int cid;
+    private int qty;
+    private double bid;
+    private String detail;
+    private String status;
+
+    public Order(int oid, int cid, int qty, double bid, String detail, String status) {
+        this.oid = oid;
+        this.cid = cid;
+        this.qty = qty;
+        this.bid = bid;
+        this.detail = detail;
+        this.status = status;
+    }
+
+    public int getOid() {
+        return oid;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public double getBid() {
+        return bid;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    @Override
+    public String toString() {
+        return "Order ID[" + oid + "]" +
+                " Status : " + status;
+    }
 }
--- Project/src/main/java/ku/cs/model/OrderList.java
+++ Project/src/main/java/ku/cs/model/OrderList.java
@@ -1,4 +1,26 @@
 package ku.cs.model;
 
+import java.util.ArrayList;
+
 public class OrderList {
+    private ArrayList<Order> orders;
+
+    public OrderList() {
+        orders = new ArrayList<>();
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public ArrayList<Order> getOrders() {
+        return this.orders;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderList{" +
+                "orders=" + orders +
+                '}';
+    }
 }
