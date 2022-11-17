78c29f4ba76c923e883ab7a1cb081e7c359a287a
B_n <78348022+boonblablabla@users.noreply.github.com>
2022-11-17 15:18:01 +0700

    Create PageChanger.java
    Router.goto now is put in a dedicated class for cleaner code

---
+++ Project/src/main/java/ku/cs/service/PageChanger.java
@@ -0,0 +1,18 @@
+package ku.cs.service;
+
+import ku.cs.Router;
+
+import java.io.IOException;
+
+public class PageChanger {
+
+
+    public static void gotoPage(String toPage) {
+        try {
+            Router.goTo(toPage);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Can't go to " + toPage);
+        }
+    }
+}
