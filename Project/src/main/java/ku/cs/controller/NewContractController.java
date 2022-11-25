52066f9c020f93b2fd0f542cce9e149e003c8642
B_n <78348022+boonblablabla@users.noreply.github.com>
2022-11-26 06:38:06 +0700

    Make NewContract handle more invalid inputs
    Customer ID and value size (contract length and deposit) are now handled properly

--- Project/src/main/java/ku/cs/controller/NewContractController.java
+++ Project/src/main/java/ku/cs/controller/NewContractController.java
@@ -1,5 +1,7 @@
 package ku.cs.controller;
 
+import com.mysql.cj.conf.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -10,7 +12,11 @@ import ku.cs.service.PageChanger;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static ku.cs.controller.LoginController.connection;
 
@@ -27,72 +33,112 @@ public class NewContractController {
     @FXML private TextField depositTextField;
 
     @FXML private void initialize() {
-        customer = CustomerDetailController.getCustomer();
-        if (customer==null) {
-            cIdTextField.clear();
-            return;
-        }
 
-        cIdTextField.setText(String.valueOf(customer.getId()));
+        // Check for user ID from CustomerDetail
+        customer = CustomerDetailController.getCustomer();
+        if (customer==null)
+            cIdTextField.clear();
+        else
+            cIdTextField.setText(String.valueOf(customer.getId()));
     }
 
     @FXML private void handleAddNewContractButton(ActionEvent event) {
         resetTagAndMessage();
-        boolean validInput = true;
-        boolean validLen = true;
-        boolean validDeposit = true;
-        boolean validID = true;
+        /*
+            index 0 -> check empty input
+            index 1 -> check non-numeric input
+            index 2 -> check valid customer ID
+            index 3 -> check valid input size
+         */
+        List<Boolean> check = Arrays.asList(true, true, false, true);
 
         // check for empty input
         if (cIdTextField.getText().isBlank()) {
-            validInput = false;
+            check.set(0, false);
             cIdTag.setText(TAG);
         }
         if (conLenTextField.getText().isBlank()) {
-            validInput = false;
+            check.set(0, false);
             conLenTag.setText(TAG);
         }
         if (depositTextField.getText().isBlank()) {
-            validInput = false;
+            check.set(0, false);
             depositTag.setText(TAG);
         }
-        if (!validInput) {
-            newContractMessageLabel.setText("Invalid contract");
+
+        if (!check.get(0)) {
+            newContractMessageLabel.setText("Input field must not be empty");
             return;
         }
 
         int cid = 0;
         int depos = 0;
         int len = 0;
+
+        // check for non-numeric input
         try {
             cid = Integer.parseInt(cIdTextField.getText());
         } catch (IllegalArgumentException e) {
-            validID = false;
+            check.set(1, false);
+            cIdTag.setText(TAG);
         }
         try {
             depos = Integer.parseInt(depositTextField.getText());
         } catch (IllegalArgumentException e) {
-            validDeposit = false;
+            check.set(1, false);
+            depositTag.setText(TAG);
         }
         try {
             len = Integer.parseInt(conLenTextField.getText());
         } catch (IllegalArgumentException e) {
-            validLen = false;
+            check.set(1, false);
+            conLenTag.setText(TAG);
         }
 
-        if (!validID) {
-            cIdTag.setText(TAG);
-            newContractMessageLabel.setText("Invalid ID");
+        if (!check.get(1)) {
+            newContractMessageLabel.setText("Input field can only contain numeric values");
             return;
         }
-        if (!validDeposit) {
+
+        // check for Customer ID
+        try {
+            String sqlText = "SELECT `C_ID` FROM customer ";
+            PreparedStatement pst = connection.prepareStatement(sqlText);
+            ResultSet result = pst.executeQuery();
+
+            while (result.next()) {
+                int cidDB = result.getInt(1);
+                if (cid == cidDB) {
+                    check.set(2, true);
+                    result.close();
+                    pst.close();
+                    break;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (!check.get(2)) {
+                newContractMessageLabel.setText("Customer not found");
+                cIdTag.setText(TAG);
+                return;
+            }
+        }
+
+        // check deposit value
+        if (depos <= 0 || depos > 1000000) {
+            check.set(3, false);
             depositTag.setText(TAG);
-            newContractMessageLabel.setText("Invalid deposit");
-            return;
         }
-        if (!validLen) {
+
+        // check contract length value
+        if (len <= 0 || len > 365) {
+            check.set(3, false);
             conLenTag.setText(TAG);
-            newContractMessageLabel.setText("Invalid length");
+        }
+
+        if (!check.get(3)) {
+            newContractMessageLabel.setText("Value range out of bound");
             return;
         }
 
