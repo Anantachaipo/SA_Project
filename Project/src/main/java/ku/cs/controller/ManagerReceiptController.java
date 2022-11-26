5603a0c4343e1bed528e8e1f53543882de8868af
B_n <78348022+boonblablabla@users.noreply.github.com>
2022-11-26 23:23:50 +0700

    Change UI for ManagerReceipt
    ReceiptList now show with reverse order

--- Project/src/main/java/ku/cs/controller/ManagerReceiptController.java
+++ Project/src/main/java/ku/cs/controller/ManagerReceiptController.java
@@ -9,6 +9,9 @@ import ku.cs.service.Utilities;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static ku.cs.controller.LoginController.connection;
 
@@ -22,18 +25,21 @@ public class ManagerReceiptController {
 
     private void showReceipt() {
         try {
+            ArrayList<Receipt> receiptList = new ArrayList<>();
+
             String sqlText = "SELECT * FROM `receipt`";
             PreparedStatement pst = connection.prepareStatement(sqlText);
             ResultSet result = pst.executeQuery();
 
             while (result.next()) {
-                Receipt receipt = new Receipt(
+                receiptList.add(new Receipt(
                         result.getInt(1),
                         result.getInt(2),
                         result.getInt(3)
-                );
-                receiptListView.getItems().add(receipt);
+                ));
             }
+            Collections.reverse(receiptList);
+            receiptListView.getItems().addAll(receiptList);
             result.close();
             pst.close();
         } catch (SQLException e) {
