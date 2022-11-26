60030ad97776f4f0d966cb2c856f4e79c6231be1
B_n <78348022+boonblablabla@users.noreply.github.com>
2022-11-27 05:14:13 +0700

    Update Utilities.java
    - Add thousandSeparator for long value

--- Project/src/main/java/ku/cs/service/Utilities.java
+++ Project/src/main/java/ku/cs/service/Utilities.java
@@ -36,6 +36,26 @@ public class Utilities {
         return sb.reverse().toString();
     }
 
+    public static String thousandSeparator(long v) {
+        String s = String.valueOf(v);
+        // value is less than a thousand, return input
+        if (s.length() <= 3) return s;
+
+        // value is more than a thousand
+        StringBuilder sb = new StringBuilder();
+        int count = 0, r;
+        for (int i=s.length(); i>0; i--) {
+            r= (int) (v%10);
+            v=v/10;
+            count++;
+            if (count%3==0 && i-1>0)
+                sb.append(r).append(",");
+            else
+                sb.append(r);
+        }
+        return sb.reverse().toString();
+    }
+
     public static String phoneSeparator(String s) {
 
         StringBuilder sb = new StringBuilder(s);
