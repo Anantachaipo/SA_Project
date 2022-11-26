a5cabe8bb9f624b309c6e96c7d56c09a1ceaa23c
B_n <78348022+boonblablabla@users.noreply.github.com>
2022-11-27 06:38:06 +0700

    Update Register UI, logic and error message
    - Added prompt texts
    - Added new logics and error messages

--- Project/src/main/java/ku/cs/controller/RegisterController.java
+++ Project/src/main/java/ku/cs/controller/RegisterController.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static ku.cs.controller.LoginController.connection;
+import static ku.cs.controller.LoginController.user;
 
 public class RegisterController {
     private static final String TAG = "*";
@@ -47,104 +48,138 @@ public class RegisterController {
     void handleRegisterButton(ActionEvent event) {
         resetTagAndMessage();
 
+        /*
+            * -> check empty input
+            * -> check non-numeric input (telephone)
+            * -> check result value doesn't exceed DB size
+                ** name     (50)
+                ** username (30)
+                ** telephone (10)
+                ** password (20)
+            * -> check duplicated username
+            * -> check password match
+        */
+
         String name = nameTextField.getText();
         String username = usernameTextField.getText();
         String tel = telTextFleld.getText();
         String password = passwordField.getText();
         String conPassword = conPasswordField.getText();
 
-        boolean validRegis = true;
-        boolean validUsername = false;
-        boolean validPassword;
+        boolean checkBlank = true;
 
         // check empty input
         if (name.isBlank()) {
             nameTag.setText(TAG);
-            validRegis = false;
+            checkBlank = false;
         }
         if (username.isBlank()) {
             usernameTag.setText(TAG);
-            validRegis = false;
+            checkBlank = false;
         }
-        // Thailand phone len range 9 - 10
-        try {
-            Integer.parseInt(tel);
-            if (tel.isBlank() || !(tel.length() == 9 || tel.length() == 10)){
-                telTag.setText(TAG);
-                validRegis = false;
-            }
-        } catch (IllegalArgumentException e) {
+        if (tel.isBlank()) {
             telTag.setText(TAG);
-            validRegis = false;
+            checkBlank = false;
         }
         if (password.isBlank()) {
             passwordTag.setText(TAG);
-            validRegis = false;
+            checkBlank = false;
         }
         if (conPassword.isBlank()) {
             conPasswordTag.setText(TAG);
-            validRegis = false;
+            checkBlank = false;
+        }
+        if (!checkBlank) {
+            registerMessageLabel.setText("Input field must not be empty");
+            return;
         }
 
-        // if input is not blank, check username
-        if (validRegis) {
-
-            // check duplicate username
-            try {
-                String sqlText = "select * FROM customer WHERE C_Username = ?";
-                PreparedStatement pst = connection.prepareStatement(sqlText);
-                pst.setString(1, username);
-                ResultSet result = pst.executeQuery();
-
-                if (result.next()) {
-                    validUsername = false;
-                    result.close();
-                    pst.close();
-                } else {
-                    validUsername = true;
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+        // check non-numeric input (telephone)
+        try {
+            int localtel = Integer.parseInt(tel);
+            if (localtel <= 0) {
+                telTag.setText(TAG);
+                registerMessageLabel.setText("Input field can only contain numeric values");
+                return;
             }
+        } catch (IllegalArgumentException e) {
+            telTag.setText(TAG);
+            registerMessageLabel.setText("Input field can only contain numeric values");
+            return;
+        }
 
+        /*
+         * check result value doesn't exceed DB size
+         ** name     (50)
+         ** username (30)
+         ** telephone (10)
+         ** password (20)
+        */
+        if (name.length() > 50) {
+            nameTag.setText(TAG);
+            registerMessageLabel.setText("Characters input limit exceeded (50)");
+            return;
+        } else if (username.length() > 30) {
+            usernameTag.setText(TAG);
+            registerMessageLabel.setText("Characters input limit exceeded (30)");
+            return;
+        } else if (!(tel.length() == 9 || tel.length() == 10)) {
+            telTag.setText(TAG);
+            registerMessageLabel.setText("Invalid Telephone number");
+            return;
+        } else if (password.length() > 20) {
+            passwordTag.setText(TAG);
+            registerMessageLabel.setText("Characters input limit exceeded (20)");
+            return;
+        }
+
+        // check duplicate username
+        try {
+            String sqlText = "select * FROM customer WHERE C_Username = ?";
+            PreparedStatement pst = connection.prepareStatement(sqlText);
+            pst.setString(1, username);
+            ResultSet result = pst.executeQuery();
+
+            if (result.next()) {
+                usernameTag.setText(TAG);
+                registerMessageLabel.setText("Username is already used");
+
+                result.close();
+                pst.close();
+                return;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
         // check password match
-        validPassword = password.equals(conPassword);
-
-        if (validRegis && validUsername && validPassword) {
-            try {
-                String sqlText = "insert into customer (C_Name, C_Username, C_Password, C_Tel) values (?,?,?,?)";
-                PreparedStatement pst = connection.prepareStatement(sqlText);
-
-                pst.setString(1, name);
-                pst.setString(2, username);
-                pst.setString(3, password);
-                pst.setString(4, tel);
-                pst.executeUpdate();
-
-                pst.close();
-
-                registerMessageLabel.setText("Register successfully");
-                Utilities.gotoPage("login");
-            } catch (SQLException e) {
-                System.err.println("ใช้ SQL ไม่ได้");
-                registerMessageLabel.setText("Invalid Register");
-                e.printStackTrace();
-            }
-        } else {
-            if (!validRegis)
-                registerMessageLabel.setText("Invalid information");
-            else if (!validUsername) {
-                registerMessageLabel.setText("Username is already used");
-                usernameTag.setText(TAG);
-            }
-            else if (!validPassword) {
-                registerMessageLabel.setText("Password is not matched");
-                passwordTag.setText(TAG);
-                conPasswordTag.setText(TAG);
-            }
+        if (!password.equals(conPassword)) {
+            registerMessageLabel.setText("Unmatched password");
+            passwordTag.setText(TAG);
+            conPasswordTag.setText(TAG);
+            return;
         }
+
+        try {
+            String sqlText = "insert into customer (C_Name, C_Username, C_Password, C_Tel) values (?,?,?,?)";
+            PreparedStatement pst = connection.prepareStatement(sqlText);
+
+            pst.setString(1, name);
+            pst.setString(2, username);
+            pst.setString(3, password);
+            pst.setString(4, tel);
+            pst.executeUpdate();
+
+            pst.close();
+
+            registerMessageLabel.setText("Register successfully");
+            Utilities.gotoPage("login");
+        } catch (SQLException e) {
+            System.err.println("ใช้ SQL ไม่ได้");
+            registerMessageLabel.setText("Invalid Register, contract application provider");
+            e.printStackTrace();
+        }
+
     }
 
     @FXML
