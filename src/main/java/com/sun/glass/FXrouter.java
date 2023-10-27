65e065e2f48ca75238006e68e4382e5a62c58604
Anantachaipo <anantachai.k@ku.th>
2023-10-27 12:30:38 +0700

    user_model and controller model

---
+++ src/main/java/com/sun/glass/FXrouter.java
@@ -0,0 +1,4 @@
+package com.sun.glass;
+
+public class FXrouter {
+}
--- src/main/java/ku/cs/controllers/UserLoginController.java
+++ src/main/java/ku/cs/controllers/UserLoginController.java
@@ -2,10 +2,90 @@ package ku.cs.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import ku.cs.model.User;
+import ku.cs.service.Account;
+//import ku.cs.service.Account;
 
 import java.io.IOException;
 
 public class UserLoginController {
+
+//    public static Officer officer;
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private PasswordField passwordPasswordField;
+
+    @FXML
+    private Label loginLabel;
+
+
+    public static User user ;
+
+    private Account account = new Account();
+
+    @FXML
+    public void login () throws IOException{
+        String userName = usernameTextField.getText();
+        String password = passwordPasswordField.getText();
+        loginLabel.setText("ไม่สามารถ login ได้ โปรดลองอีกครั้ง");
+        user = account.checkUserName(userName,password);
+        if (user != null) {
+            try{
+                com.github.saacsos.FXRouter.goTo("user_menu");
+            } catch (IOException e) {
+                System.err.println("ไปที่หน้า ีuser_menu ไม่ได้");
+                System.err.println("ตรวจสอบความถูกต้องของ username และ password");
+                System.err.println("ให้ตรวจสอบการกำหนด router");
+            }
+        }
+
+
+
+
+
+
+
+
+//                if (user instanceof Admin) {
+//                    try {
+//                        com.github.saacsos.FXRouter.goTo("admin_main");
+//                    } catch (IOException e) {
+//                        System.err.println("ไปที่หน้า admin_main ไม่ได้");
+//                        System.err.println("ให้ตรวจสอบ Username หรือ Password");
+//                        System.err.println("ให้ตรวจสอบการกำหนด route");
+//                    }
+//                } else if (user instanceof Officer) {
+//                    officer = (Officer) user;
+//                    try {
+//                        com.github.saacsos.FXRouter.goTo("officer_main");
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                        System.err.println("ไปที่หน้า officer_main ไม่ได้");
+//                        System.err.println("ให้ตรวจสอบ Username หรือ Password");
+//                        System.err.println("ให้ตรวจสอบการกำหนด route");
+//                    }
+//                } else {
+//                    try {
+//                        com.github.saacsos.FXRouter.goTo("menu");
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                        System.err.println("ไปที่หน้า menu ไม่ได้");
+//                        System.err.println("ให้ตรวจสอบ Username หรือ Password");
+//                        System.err.println("ให้ตรวจสอบการกำหนด route");
+//                    }
+//                }
+//            }else{
+//
+//                loginLabel.setText("บัญชีของคุณถูกระงับการใช้งาน");
+//            }
+//
+//        }
+    }
 
     @FXML
     public void backToUserLogin(ActionEvent actionEvent) {
